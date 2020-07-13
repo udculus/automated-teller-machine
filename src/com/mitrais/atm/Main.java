00fd02b19872238f237a3ccfdc38f2a26c5bc099
DESKTOP-M3A8N5E\Yudhi Guntara <yudhi.guntara@gmail.com>
2020-07-13 11:04:57 +0700

    Added inital screen and data seeding

--- src/com/mitrais/atm/Main.java
+++ src/com/mitrais/atm/Main.java
@@ -1,8 +1,18 @@
 package com.mitrais.atm;
 
+import com.mitrais.atm.dao.AccountDaoImpl;
+import com.mitrais.atm.screen.LoginScreen;
+
 public class Main {
 
+    /**
+     * Seed account and show login screen
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("ATM Simulation");
+        AccountDaoImpl accountDao = AccountDaoImpl.getInstance();
+        accountDao.seedAccounts();
+
+        LoginScreen.getInstance().show();
     }
 }
