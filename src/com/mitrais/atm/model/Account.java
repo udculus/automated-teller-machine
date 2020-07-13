79ae23999ea29f045425d6631a1c869c9237b0ff
DESKTOP-M3A8N5E\Yudhi Guntara <yudhi.guntara@gmail.com>
2020-07-13 11:06:05 +0700

    Added model for account and screen validation

---
+++ src/com/mitrais/atm/model/Account.java
@@ -0,0 +1,58 @@
+package com.mitrais.atm.model;
+
+public class Account {
+
+    private String accountNumber;
+    private String pin;
+    private String name;
+    private int balance;
+
+    public Account(String accountNumber, String pin, String name, int balance) {
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", pin='" + pin + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
---
+++ src/com/mitrais/atm/model/Validation.java
@@ -0,0 +1,30 @@
+package com.mitrais.atm.model;
+
+public class Validation {
+
+    private boolean isValid = true;
+    private String message;
+
+    public Validation() {}
+
+    public Validation(boolean isValid, String message) {
+        this.isValid = isValid;
+        this.message = message;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
